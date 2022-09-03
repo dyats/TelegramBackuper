5a1843d47f90c5f484b9bbd77dddc07e2faa5342
Denny <comdenchik@gmail.com>
2022-09-03 20:59:26 +0300

    migrated to new html page format and refactored the code

--- OrderGiv3r.VideoDownloader/HtmlExtensions.cs
+++ OrderGiv3r.VideoDownloader/HtmlExtensions.cs
@@ -6,19 +6,9 @@ namespace OrderGiv3r.VideoDownloader;
 
 public static class HtmlExtensions
 {
-    public static string GetHtmlComponent(this HtmlDocument document)
+    public static string GetUrlForDownload(this HtmlDocument document)
     {
         var match = Regex.Match(document.ParsedText, HtmlMatchCondition);
-        var encodedUri = match.Groups[RegexMatchedGroupId].Value;
-        var decodedHtmlComponent = HttpUtility.UrlDecode(encodedUri);
-        return decodedHtmlComponent;
-    }
-
-    public static string GetLinkFromComponent(this string decodedHtmlComponent)
-    {
-        var document = new HtmlDocument();
-        document.LoadHtml(decodedHtmlComponent);
-        var link = document.DocumentNode.SelectSingleNode(UlMatchCondition).Attributes[LiAttribute].Value;
-        return link;
+        return match.Groups[RegexMatchedGroupId].Value;
     }
 }
