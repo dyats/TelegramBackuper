6cc91d2bf4fca66d0e3018a8be43c3e199e01d1f
Denny <comdenchik@gmail.com>
2022-09-04 13:41:45 +0300

    Added configuration instead of constants for Video Downloader

--- OrderGiv3r.VideoDownloader/Program.cs
+++ OrderGiv3r.VideoDownloader/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System.Text;
 using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
 using OrderGiv3r.VideoDownloader;
 
 Console.InputEncoding = Encoding.UTF8;
 Console.OutputEncoding = Encoding.UTF8;
 
+var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+var videoNumber = 11111;
+
 var web = new HtmlWeb();
-var url = BaseUrl + VideoNumber;
+var url = config["BaseUrl"] + videoNumber;
 var document = web.Load(url);
 var downloadFromUrl = document.GetUrlForDownload();
-var pathToDownload = DesktopPath + VideoNumber + VideoFormat;
+var pathToDownload = config["PathToDownload"] + videoNumber + config["VideoFormat"];
 var client = CreateHttpClient();
 await client.DownloadFileAsync(downloadFromUrl, pathToDownload);
 
