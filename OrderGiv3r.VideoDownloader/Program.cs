e61deaf7c215609604868a830fb858f18aea159e
Denny <comdenchik@gmail.com>
2022-09-05 21:52:21 +0300

    refactored video downloader and integrated it into the bot

--- OrderGiv3r.VideoDownloader/Program.cs
+++ OrderGiv3r.VideoDownloader/Program.cs
@@ -1,24 +1,24 @@
-﻿using System.Text;
-using HtmlAgilityPack;
-using Microsoft.Extensions.Configuration;
-using OrderGiv3r.VideoDownloader;
+﻿//using System.Text;
+//using HtmlAgilityPack;
+//using Microsoft.Extensions.Configuration;
+//using OrderGiv3r.VideoDownloader;
 
-Console.InputEncoding = Encoding.UTF8;
-Console.OutputEncoding = Encoding.UTF8;
+//Console.InputEncoding = Encoding.UTF8;
+//Console.OutputEncoding = Encoding.UTF8;
 
-var config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false)
-            .Build();
+//var config = new ConfigurationBuilder()
+//            .SetBasePath(Directory.GetCurrentDirectory())
+//            .AddJsonFile("appsettings.json", optional: false)
+//            .Build();
 
-var videoNumber = 11111;
+//var videoNumber = 11111;
 
-var web = new HtmlWeb();
-var url = config["BaseUrl"] + videoNumber;
-var document = web.Load(url);
-var downloadFromUrl = document.GetUrlForDownload();
-var pathToDownload = config["PathToDownload"] + videoNumber + config["VideoFormat"];
-var client = CreateHttpClient();
-await client.DownloadFileAsync(downloadFromUrl, pathToDownload);
+//var web = new HtmlWeb();
+//var url = config["BaseUrl"] + videoNumber;
+//var document = web.Load(url);
+//var downloadFromUrl = document.GetUrlForDownload();
+//var pathToDownload = config["PathToDownload"] + videoNumber + config["VideoFormat"];
+//var client = CreateHttpClient();
+//await client.DownloadFileAsync(downloadFromUrl, pathToDownload);
 
-Console.ReadLine();
+//Console.ReadLine();
---
+++ OrderGiv3r.VideoDownloader/VideoDownloaderService.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+
+namespace OrderGiv3r.VideoDownloader;
+
+public class VideoDownloaderService
+{
+    private readonly HtmlWeb _web;
+
+    public VideoDownloaderService()
+    {
+        _web = new HtmlWeb();
+    }
+
+    public async Task DownloadVideoAsync(string url, string pathToDownload, string matchCondition, int matchedGroup)
+    {
+        var document = _web.Load(url);
+        var downloadFromUrl = document.GetUrlForDownload(matchCondition, matchedGroup);
+        var client = CreateHttpClient();
+        await client.DownloadFileAsync(downloadFromUrl, pathToDownload);
+    }
+}
